e8e486d4e7085927c27b42eba2456e3a4e7c0f33
Yuri Valença <yvc@cin.ufpe.br>
2023-05-23 14:26:31 -0300

    feature(system): :art: Update App.tsx file to add theme and font
    Signed-off-by: Yuri Valença <yvc@cin.ufpe.br>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import './App.css';
+import { ThemeProvider } from '@mui/material/styles';
 import Login from './components/LoginPage';
+import WebFont from 'webfontloader';
+import { CssBaseline } from '@mui/material';
+import { theme } from './themes/theme';
+
+WebFont.load({
+  google: {
+    families: ['Rajdhani:300,400', 'Kanit:300,400']
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <Login />
+      </div>
+    </ThemeProvider>
   );
 }
 
