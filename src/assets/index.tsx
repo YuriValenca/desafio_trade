03b85d465e6221969a4f5cebe52a00f22f0ab3a7
Yuri Valença <yvc@cin.ufpe.br>
2023-05-23 14:24:00 -0300

    style(login): :milky_way: Add images to project to be used on login page
    Signed-off-by: Yuri Valença <yvc@cin.ufpe.br>

---
+++ src/assets/index.tsx
@@ -0,0 +1,5 @@
+import questionMark from './icons/questionMarkIcon.png'
+import worldMap from './images/worldMap.png'
+
+export const questionMarkIcon = questionMark;
+export const worldMapImage = worldMap;
